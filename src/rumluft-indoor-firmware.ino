db6ea60f9739308c8e648f62c56bcd4a61f36259
mguntli <mguntli@users.noreply.github.com>
2018-04-20 11:00:51 +0200

    Extend MeasurementData with absolute humidity
    Create a struct to hold all relevant measurement sample values

--- src/rumluft-indoor-firmware.ino
+++ src/rumluft-indoor-firmware.ino
@@ -10,77 +10,63 @@ const uint8_t I2C_ADDR_SHT31D = 0x44U;
 SHT31D_CC::ClosedCube_SHT31D sht31d;
 SensirionSGP30 sgp30;
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-    Serial.println("rumluft firmware");
-    if (!sht31d.begin(I2C_ADDR_SHT31D)) {
-        Serial.println("SHT31 sensor not found");
-    }
-    if (!sgp30.begin()) {
-        Serial.println("SGP30 sensor not found");
-    }
-    if (!sgp30.IAQinit()) {
-        Serial.println("SGP30 init failed");
-    }
-}
+struct MeasurementData {
+    MeasurementData() {
+        temperature = NAN;
+        humidity = NAN;
+        voc = NAN;
+        co2 = NAN;
+        absoluteHumidity = NAN;
+    };
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-    // SHT31D
-    float temperature = NAN; // °C
-    float humidity = NAN; // %RH
-    readTemperatureHumidity(temperature, humidity);
+    float temperature; // [°C]
+    float humidity; // [%RH]
+    float voc; // [ppb]
+    float co2; // [ppm]
+    float absoluteHumidity; // [mg/m^3]
+};
 
-    // SGP30
-    setHumidityCompensation(temperature, humidity);
-    float voc = NAN; // ppb
-    float co2 = NAN; // ppm
-    readAirQuality(voc, co2);
-
-    // Pubish data to consumers (cloud and other mesh nodes)
-    publishMeasurementData(temperature, humidity, voc, co2);
-
-    // Wait until next sample
-    delay(10000);
-}
-
-void readTemperatureHumidity(float& temperature, float& humidity) {
+void readTemperatureHumidity(MeasurementData& sample) {
     SHT31D_CC::SHT31D sht31dresult = sht31d.readTempAndHumidity(SHT31D_CC::REPEATABILITY_LOW, SHT31D_CC::MODE_CLOCK_STRETCH, 50);
     if (sht31dresult.error == SHT31D_CC::NO_ERROR) {
-        temperature = sht31dresult.t;
-        humidity = sht31dresult.rh;
-        Serial.print("Temp = "); Serial.print(temperature); Serial.println(" °C");
-        Serial.print("Humidity = "); Serial.print(humidity); Serial.println(" %RH");
+        sample.temperature = sht31dresult.t;
+        sample.humidity = sht31dresult.rh;
+        Serial.print("Temp = "); Serial.print(sample.temperature); Serial.println(" °C");
+        Serial.print("Humidity = "); Serial.print(sample.humidity); Serial.println(" %RH");
     }
     else {
         Serial.println("SHT31D measurement failed");
     }
 }
 
-void setHumidityCompensation(const float temperature, const float humidity) {
-    uint32_t absoluteHumidity = getAbsoluteHumidity(temperature, humidity);
-    if (!sgp30.setHumidity(absoluteHumidity)) {
+void setHumidityCompensation(MeasurementData& sample) {
+    uint32_t absoluteHumidity = getAbsoluteHumidity(sample.temperature, sample.humidity);
+    if (sgp30.setHumidity(absoluteHumidity)) {
+        sample.absoluteHumidity = absoluteHumidity;
+        Serial.print("Absolute Humidity = "); Serial.print(sample.absoluteHumidity); Serial.println(" mg/m^3");
+    }
+    else {
         Serial.println("SGP30 setting absolute humidity failed");
     }
 }
 
-void readAirQuality(float& voc, float& co2) {
+void readAirQuality(MeasurementData& sample) {
     if (sgp30.IAQmeasure()) {
-        voc = sgp30.TVOC;
-        co2 = sgp30.eCO2;
-        Serial.print("TVOC = "); Serial.print(voc); Serial.println(" ppb");
-        Serial.print("CO2 = "); Serial.print(co2); Serial.println(" ppm");
+        sample.voc = sgp30.TVOC;
+        sample.co2 = sgp30.eCO2;
+        Serial.print("TVOC = "); Serial.print(sample.voc); Serial.println(" ppb");
+        Serial.print("CO2 = "); Serial.print(sample.co2); Serial.println(" ppm");
     }
     else {
         Serial.println("SGP30 measurement failed");
     }
 }
 
-void publishMeasurementData(const float temperature, const float humidity, const float voc, const float co2) {
+void publishMeasurementData(const MeasurementData& sample) {
     // Send as text representation in JSON format so it can easily be processed
     char data[255];
     memset(data, sizeof(data), 0);
-    snprintf(data, sizeof(data), "{\"temp\":%.2f,\"humid\":%.2f,\"voc\":%.2f,\"co2\":%.2f}", temperature, humidity, voc, co2);
+    snprintf(data, sizeof(data), "{\"temp\":%.2f,\"humid\":%.2f,\"voc\":%.2f,\"co2\":%.2f,\"absHumid\":%.2f}", sample.temperature, sample.humidity, sample.voc, sample.co2, sample.absoluteHumidity);
 
     // Trigger the integration (limited to 255 bytes)
     Particle.publish("v1-indoor-data", data, PRIVATE);
@@ -97,4 +83,35 @@ uint32_t getAbsoluteHumidity(const float temperature, const float humidity) {
     const float absoluteHumidity = 2.16679 * (waterVaporPressure * 1000 /*[Pa]*/) / (temperature + 273.15 /* [K] */); // [g/m^3]
     const uint32_t absoluteHumidityScaled = 1000 * absoluteHumidity; // [mg/m^3]
     return absoluteHumidityScaled;
+}
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+    Serial.println("rumluft firmware");
+    if (!sht31d.begin(I2C_ADDR_SHT31D)) {
+        Serial.println("SHT31 sensor not found");
+    }
+    if (!sgp30.begin()) {
+        Serial.println("SGP30 sensor not found");
+    }
+    if (!sgp30.IAQinit()) {
+        Serial.println("SGP30 init failed");
+    }
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+    MeasurementData sample;
+    // SHT31D
+    readTemperatureHumidity(sample);
+
+    // SGP30
+    setHumidityCompensation(sample);
+    readAirQuality(sample);
+
+    // Pubish data to consumers (cloud and other mesh nodes)
+    publishMeasurementData(sample);
+
+    // Wait until next sample
+    delay(10000);
 }
