d47c5da364d1a4eec35501982dbea8f7a3b514e0
mguntli <mguntli@users.noreply.github.com>
2018-06-12 13:44:33 +0200

    Automatic SGP30 baseline calibration sequence
    If the persisted baseline is is not valid or older than 1 week, the baseline will not be restored to the SGP30. This will result in the sensor trying to determine a new baseline.
    The adjustement algorithm will be accelerated for 12hrs which is the Maximum time required to find a new baseline. If the device has been operated for 12h, the baseline will be persisted.

--- src/rumluft-indoor-firmware.ino
+++ src/rumluft-indoor-firmware.ino
@@ -28,21 +28,22 @@ struct MeasurementData {
 };
 
 struct EepromStorage {
-    uint8_t version; // helper to detect if EEPROM needs to be initialized (default 0xFF for empty EEPROM)
-    bool baselineValid; // flag set once manual burn in completed
-    long timeStamp;
-    uint16_t eco2_base;
-    uint16_t tvoc_base;
+    uint8_t version; // helper value to detect if EEPROM needs to be initialized (default 0xFF for empty EEPROM)
+    bool baselineValid; // flag set once SGP30 baseline calibration sequence is completed (12 hours)
+    long timeStamp; // timestamp when baseline calibration sequence has been started or last valid baseline has been stored [Unix time format]
+    uint16_t eco2_base; // SGP30 baseline value for CO2
+    uint16_t tvoc_base; // SGP30 baseline value for TVOC
 };
 
-// global variables
+// object instances
 SHT31D_CC::ClosedCube_SHT31D sht31d;
 SensirionSGP30 sgp30;
 EepromStorage persistency;
+bool baselineCalibrationInProgress = false;
 
-/* return absolute humidity [mg/m^3] with approximation formula
-* @param temperature [°C]
-* @param humidity [%RH]
+/* Helper to calculate absolute humidity [mg/m^3] with approximation formula based on temperature [°C] and humidity [%RH].
+* @param const MeasurementData& sample
+* @return absolute humidity [mg/m^3]
 */
 uint32_t getAbsoluteHumidity(const MeasurementData& sample) {
     // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
@@ -51,6 +52,9 @@ uint32_t getAbsoluteHumidity(const MeasurementData& sample) {
     return absoluteHumidityScaled;
 }
 
+/* Read the temperature and humidity from SHT31-D and store in the given sample
+* @param MeasurementData& sample - to store temperature and humidity
+*/
 void readTemperatureHumidity(MeasurementData& sample) {
     SHT31D_CC::SHT31D sht31dresult = sht31d.readTempAndHumidity(SHT31D_CC::REPEATABILITY_LOW, SHT31D_CC::MODE_CLOCK_STRETCH, 50);
     if (sht31dresult.error == SHT31D_CC::NO_ERROR) {
@@ -64,6 +68,9 @@ void readTemperatureHumidity(MeasurementData& sample) {
     }
 }
 
+/* Set the absolute humidity on the SGP30 for more acurate readings of air quality signals (TVOC & eCO2) and store absolute humidity in the given sample.
+* @param MeasurementData& sample - to store absolute humidity value
+*/
 void setHumidityCompensation(MeasurementData& sample) {
     uint32_t absoluteHumidity = getAbsoluteHumidity(sample);
     if (sgp30.setHumidity(absoluteHumidity)) {
@@ -75,6 +82,9 @@ void setHumidityCompensation(MeasurementData& sample) {
     }
 }
 
+/* Read the IAQ signals from SGP30 and store VOC and CO2 in the given sample.
+* @param MeasurementData& sample - to store voc and co2 values
+*/
 void readAirQuality(MeasurementData& sample) {
     if (sgp30.IAQmeasure()) {
         sample.voc = sgp30.TVOC;
@@ -87,6 +97,9 @@ void readAirQuality(MeasurementData& sample) {
     }
 }
 
+/* Read the baseline from SGP30 and store eco2_base and tvoc_base in the given sample.
+* @param MeasurementData& sample - to store eco2_base and tvoc_base values
+*/
 void readBaseline(MeasurementData& sample) {
     uint16_t eco2_base, tvoc_base;
     if (sgp30.getIAQBaseline(&eco2_base, &tvoc_base)) {
@@ -100,6 +113,8 @@ void readBaseline(MeasurementData& sample) {
     }
 }
 
+/* Restore the baseline if it's valid and less than 1 week old. In case of an old baseline, the timestamp for the baseline calibration sequence is stored.
+*/
 void restoreBaseline() {
     EEPROM.get(0, persistency);
     const bool baselineValid = persistency.baselineValid;
@@ -113,16 +128,34 @@ void restoreBaseline() {
             Serial.println("SGP30 baseline setting failed");
         }
     }
+    else {
+        Serial.println("Warning - Baseline calibration (12h) required!");
+        persistency.baselineValid = false;
+        persistency.timeStamp = Time.now();
+        baselineCalibrationInProgress = true;
+    }
 }
 
 void refreshBaseline(const MeasurementData& sample) {
-    // Once the baseline is properly initialized or restored, the current baseline value should be stored approximately once per hour
     EEPROM.get(0, persistency);
     const bool baselineValid = persistency.baselineValid;
     const long ONE_HOUR_MILLIS = 60 * 60 * 1000;
     const bool olderThan1Hour = (Time.now() - persistency.timeStamp) >= ONE_HOUR_MILLIS;
     if (baselineValid && olderThan1Hour) {
+        // Once the baseline is properly initialized or restored, the current baseline value should be stored approximately once per hour
         persistBaseline(sample.eco2_base, sample.tvoc_base);
+    }
+    else if (baselineCalibrationInProgress) {
+        // Restarting the sensor without restoring a baseline will result in the sensor trying to determine a new baseline.
+        // The baseline calibration algorithm will be performed on the SGP30 for 12hrs (maximum time required to find a new baseline).
+        const bool baselineCalibrationCompleted = (Time.now() - persistency.timeStamp) >= (12 * ONE_HOUR_MILLIS);
+        if (baselineCalibrationInProgress && baselineCalibrationCompleted) {
+            // The sensor adjusts to the best value it has been exposed to. So keeping it indoors the sensor thinks this is the best value and sets it to 0ppb tVOC and 400ppm CO2eq.
+            // As soon as you expose the sensor to outside air it can adjust to the global H2 Background Signal. For normal Operation exposing the sensor to outside air for 10min cumulative time should be sufficient.
+            Serial.println("SGP30 baseline calibration completed");
+            persistBaseline(sample.eco2_base, sample.tvoc_base);
+            baselineCalibrationInProgress = false;
+        }
     }
 }
 
@@ -169,6 +202,18 @@ int cloudFunctionSetBaseline(String extra) {
     return -1;
 }
 
+// Cloud functions must return int and take one String
+int cloudFunctionClearBaseline(String extra) {
+    // Note: requires a power sequence to properly reinitialize SGP30 baseline calibration sequence
+    persistency.version = 0xFF;
+    persistency.baselineValid = false;
+    persistency.timeStamp = 0;
+    persistency.eco2_base = 0;
+    persistency.tvoc_base = 0;
+    EEPROM.put(0, persistency);
+    return 0;
+}
+
 // setup() runs once, when the device is first turned on.
 void setup() {
     Serial.println("rumluft firmware");
@@ -187,6 +232,7 @@ void setup() {
 
     // register the cloud function (Up to 15 cloud functions may be registered and each function name is limited to a maximum of 12 characters)
     Particle.function("setBaseline", cloudFunctionSetBaseline);
+    Particle.function("clrBaseline", cloudFunctionClearBaseline);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
